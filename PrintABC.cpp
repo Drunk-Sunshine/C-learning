0203afb5e349462de7bed6e7b950496711ca6f57
drunksunshine <591485561@qq.com>
2024-09-27 17:45:32 +0800

    三线程循环打印ABC

---
+++ PrintABC.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+using namespace std;
+
+class PrintABC {
+private:
+	int times; // 控制打印次数
+	atomic<int> state{ 0 }; // 当前状态值，保证交替打印
+	mutex _lock;
+
+public:
+	PrintABC(int times) : times(times) {}
+
+	void printLetter(const string& name, int targetNum) {
+		for (int i = 0; i < times;) {
+			{
+				unique_lock<mutex> lock(_lock);
+				if (state % 3 == targetNum) {
+					state++;
+					i++;
+					cout << name;
+				}
+			}
+		}
+	}
+};
+
+int main() {
+	PrintABC loopthread(5);
+
+	thread tA(&PrintABC::printLetter, &loopthread, "A", 0);
+	thread tB(&PrintABC::printLetter, &loopthread, "B", 1); // 线程 B  
+	thread tC(&PrintABC::printLetter, &loopthread, "C", 2); // 线程 C
+
+	tA.join();
+	tB.join();
+	tC.join();
+
+	return 0;
+}
